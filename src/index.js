d43485902127a5d5683b93f66f742b9048f3064c
PolinaPuzhevich <polinlukyanenko@gmail.com>
2024-10-23 15:35:53 +0300

    feat: complete task in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let nAbs = Math.abs(n);
+    let nToString = nAbs.toString();
+    let i = nToString.length;
+  let result = '';
+  while (i > 0) {
+    result = `${result}${nToString[i - 1]}`;
+    i = i - 1;
+  }
+  return result;
 }
